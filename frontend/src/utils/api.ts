1b8507358537ba4bf44f468a2e0d5a351a471e79
The-YashGaur <gauryash0904@gmail.com>
2025-10-11 23:10:48 +0530

    commit added

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://alltheclasses-yoga.onrender.com/api';
 
 export const submitContactForm = async (formData: {
   name: string;
